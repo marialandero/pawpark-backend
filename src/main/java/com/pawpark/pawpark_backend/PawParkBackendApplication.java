3ff9aaeae28d66d3038ae4b6260eaf7c54800f1a
María Landero Contreras <marialandero96@gmail.com>
2026-02-11 20:39:55 +0100

    first commit

---
+++ src/main/java/com/pawpark/pawpark_backend/PawParkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pawpark.pawpark_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawParkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawParkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawpark/pawpark_backend/PawParkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpark.pawpark_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawParkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
